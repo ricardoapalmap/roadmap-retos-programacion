953f7fef17cdf4dfac0fbaa36dedc2a6096d2946
Ricardo Andrés Palma <ricardo.palma04@gmail.com>
2024-06-05 14:52:12 -0400

    #00 -javascript

---
+++ #00 SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/ricardoapalmap.js
@@ -0,0 +1,38 @@
+// EJERCICIO:
+// 1.-Crea un comentario en el código y coloca la URL del sitio web oficial del lenguaje de programación que has seleccionado.
+
+// https://javascript.com
+
+// 2.-Representa las diferentes sintaxis que existen de crear comentarios en el lenguaje (en una línea, varias...).
+
+// Comentario de una sola Línea.
+
+/* Comentario
+   de
+   varias
+   Líneas
+
+/* Comentarios
+   de varias
+   líneas
+   encerrados
+*/
+
+// 3.-Crea una variable (y una constante si el lenguaje lo soporta).
+
+var miNombre = "Ricardo"
+var miNombre2 = "Andrés"
+const miConstante = "Esto es una constante"
+
+// 4.-Crea variables representando todos los tipos de datos primitivos del lenguaje (cadenas de texto, enteros, booleanos...).
+
+var númeroEntero = 99; // Integer
+var númeroDécimal = 23.5; // Float
+var textoCadena = "Esto es un texto cadena"; // String
+var boleano1 = true; // Boolean
+var boleano2 = false; // Boolean
+var colecciónDeVariables = ["valor1","valor2","valor3"]; // Arrays
+
+// 5.-Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+
+console.log("¡Hola, JavaScript!")
